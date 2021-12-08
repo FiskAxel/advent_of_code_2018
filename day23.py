c55317c614a7cc5d5e7ae8f0423d5cca45944e32
FiskAxel <awite_sundin@hotmail.com>
2021-12-08 16:05:15 +0100

    Revert "Revert "update""
    This reverts commit 483e820c1549f9d286cc4b2dbb450615a13e5770.

---
+++ day23.py
@@ -0,0 +1,11 @@
+def main():
+	with open('test.txt', 'r') as puzzleInput:
+		input = puzzleInput.readlines()
+		result = 0
+		print(f"Part 1: {result}")
+		print(f"Part 2: {result}")
+
+def function():
+	return
+
+main()
